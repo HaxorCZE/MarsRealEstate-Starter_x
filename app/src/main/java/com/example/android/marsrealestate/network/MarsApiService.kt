65b0571bf97133d32c15ec4703613b0fa79e49fb
Michal <hexalp@gmail.com>
2021-12-13 23:53:59 +0100

    Oprava chyb podruhe :D

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,15 +17,18 @@
 
 package com.example.android.marsrealestate.network
 
-import retrofit2.Call
+import com.squareup.moshi.Moshi
 import retrofit2.Retrofit
 import retrofit2.http.GET
+import retrofit2.Call
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import com.squareup.moshi.Moshi
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
+
+private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
 
 private val retrofit = Retrofit.Builder()
         .addConverterFactory(MoshiConverterFactory.create(moshi))
@@ -41,8 +44,3 @@ object MarsApi {
         val retrofitService : MarsApiService by lazy {
                 retrofit.create(MarsApiService::class.java) }
 }
-
-private val moshi = Moshi.Builder()
-        .add(KotlinJsonAdapterFactory())
-        .build()
-
